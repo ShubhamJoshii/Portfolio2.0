ffd799c6ec8719df023b34295e6d72372a6982c2
ShubhamJoshii <shubhamjoshii676@gmail.com>
2022-10-17 20:30:47 +0530

    Skill Hover Message Added

--- src/AllData.jsx
+++ src/AllData.jsx
@@ -10,7 +10,7 @@ import Password from "./Images/ProjectPassword.png";
 import Hotstar from "./Images/ProjectHotstar.png";
 import DigitalClock from "./Images/ProjectDigital.png";
 import formore from "./Images/Click.png";
-import Shubham2 from "./Images/Shubham2.jpg"
+import Shubham2 from "./Images/Shubham2.jpg";
 const Data = {
   SocialLogo: [
     {
@@ -18,7 +18,7 @@ const Data = {
       logo: Insta,
       link: "https://www.instagram.com/invites/contact/?i=1k3g7gxwflgz0&utm_content=2of27u2",
     },
-    
+
     {
       text: "Facebook",
       logo: Facebook,
@@ -41,51 +41,123 @@ const Data = {
     },
   ],
   MainPageImg: FrontImg,
-  resume:"https://drive.google.com/file/d/170t40CWF0c0pLFkfXWJmqu7MMjPYWeO6/view?usp=sharing",
+  resume:
+    "https://drive.google.com/file/d/170t40CWF0c0pLFkfXWJmqu7MMjPYWeO6/view?usp=sharing",
   Shubham2,
   Project: [
     {
       text: "Portfolio",
       Image: Portfolio,
       link: "https://shubhamjoshii.github.io/Portfolio2.0/",
-      para:"Language Use ",
-      LanguageUse:["HTML", "CSS", "React JS"]
+      para: "Language Use ",
+      LanguageUse: ["HTML", "CSS", "React JS"],
     },
     {
       text: "Hotstar Clone",
 
       Image: Hotstar,
       link: "https://shubhamjoshii.github.io/Hotstar-Clone/",
-      para:"Language Use ",
-      LanguageUse:["HTML", "CSS", "React JS"]
+      para: "Language Use ",
+      LanguageUse: ["HTML", "CSS", "React JS"],
     },
     {
       text: "Quotes Generator",
       Image: Quotes,
       link: "https://shubhamjoshii.github.io/quotesGenerator/",
-      para:"Language Use ",
-      LanguageUse:["HTML", "CSS", "React JS"]
+      para: "Language Use ",
+      LanguageUse: ["HTML", "CSS", "React JS"],
     },
     {
       text: "Digital Clock",
       Image: DigitalClock,
       link: "https://shubhamjoshii.github.io/Digital-Clock/",
-      para:"Language Use ",
-      LanguageUse:["HTML","CSS", "React JS"]
+      para: "Language Use ",
+      LanguageUse: ["HTML", "CSS", "React JS"],
     },
     {
       text: "Password Generator",
       Image: Password,
       link: "https://shubhamjoshii.github.io/PasswordGenerator/",
-      para:"Language Use ",
-      LanguageUse:["HTML", "CSS", "React JS"]
+      para: "Language Use ",
+      LanguageUse: ["HTML", "CSS", "React JS"],
     },
     {
       text: "For more Click Here!",
       Image: formore,
-      link: "https://github.com/ShubhamJoshii"
+      link: "https://github.com/ShubhamJoshii",
     },
   ],
+  TopicData: {
+    Skills: [
+      {
+        Topic: "Web Development",
+        SubTopic: "Web App Development",
+
+        HoverTopic1: "HTML",
+        HoverTopic1Perc: "95%",
+        HoverTopic2: "CSS",
+        HoverTopic2Perc: "85%",
+        HoverTopic3: "JS",
+        HoverTopic3Perc: "70%",
+        HoverTopic4: "React JS",
+        HoverTopic4Perc: "75%",
+      },
+      {
+        Topic: "UI/UX",
+        SubTopic: "Designing Web Interfaces",
+        HoverTopic1: "Figma",
+        HoverTopic1Perc: "75%",
+        HoverTopic2: "Adobe xD",
+        HoverTopic2Perc: "70%",
+        HoverTopic3: "Adobe Photoshop",
+        HoverTopic3Perc: "10%",
+        HoverTopic4: "Canva",
+        HoverTopic4Perc: "40%",
+      },
+      {
+        Topic: "App Development",
+        SubTopic: "Buliding Android Apps ",
+        HoverTopic1: "Python",
+        HoverTopic1Perc: "80%",
+        HoverTopic2: "C",
+        HoverTopic2Perc: "60%",
+        HoverTopic3: "C++",
+        HoverTopic3Perc: "30%",
+        HoverTopic4: "JS",
+        HoverTopic4Perc: "70%",
+      },
+    ],
+    Experience: [
+      {
+        Topic: "2021 - Current",
+        SubTopic: "Freelancer",
+      },
+      {
+        Topic: "2022",
+        SubTopic: "Certification On Data Structure On Python by NPTEL",
+      },
+      {
+        Topic: "2022",
+        SubTopic:
+          "Front-End Web Development From Google Developer Student Clubs and Microsoft Learn Student Ambassadors Conducted by Devtown ",
+      },
+    ],
+    Education: [
+      {
+        Topic: "2021 - Current",
+        SubTopic:
+          "Bachelor of Computer Applications(BCA) at Vivekananda Institute of Professional Studies",
+      },
+      {
+        Topic: "2020 - 2021",
+        SubTopic: "Schooling at Kendriya Vidyalaya",
+      },
+      {
+        Topic: "2018 - 2019",
+        SubTopic: "Schooling at Kendriya Vidyalaya",
+      },
+    ],
+  },
 };
 
 export default Data;
--- src/ContentPage3.jsx
+++ src/ContentPage3.jsx
@@ -1,56 +1,15 @@
-import Data from "./AllData";
 import React, { useState } from "react";
-
-const TopicData = {
-  Skills: [
-    {
-      Topic: "Web Development",
-      SubTopic: "Web App Development",
-    },
-    {
-      Topic: "UI/UX",
-      SubTopic: "Designing Web Interfaces",
-    },
-    {
-      Topic: "App Development",
-      SubTopic: "Buliding Android Apps ",
-    },
-  ],
-  Experience: [
-    {
-      Topic: "2021 - Current",
-      SubTopic: "Freelancer",
-    },
-    {
-      Topic : "2022",
-      SubTopic : "Certification On Data Structure On Python by NPTEL"
-    },
-    {
-      Topic : "2022",
-      SubTopic : "Front-End Web Development From Google Developer Student Clubs and Microsoft Learn Student Ambassadors Conducted by Devtown "
-    }
-  ],
-  Education: [
-    {
-      Topic: "2021 - Current",
-      SubTopic:
-        "Bachelor of Computer Applications(BCA) at Vivekananda Institute of Professional Studies",
-    },
-    {
-      Topic: "2020 - 2021",
-      SubTopic: "Schooling at Kendriya Vidyalaya",
-    },
-    {
-      Topic: "2018 - 2019",
-      SubTopic: "Schooling at Kendriya Vidyalaya",
-    },
-  ],
-};
+import Data from "./AllData";
 
 const ContentPage3 = () => {
   // let i = TopicData.Skills;
   const [Count, setCount] = useState(0);
-  let TopicArr = [TopicData.Skills, TopicData.Experience, TopicData.Education];
+  let TopicArr = [
+    Data.TopicData.Skills,
+    Data.TopicData.Experience,
+    Data.TopicData.Education,
+  ];
+  console.log(Data.TopicData);
   return (
     <div className="ContentPage3" id="AboutMe">
       <div className="Page3First">
@@ -79,8 +38,10 @@ const ContentPage3 = () => {
           <button
             id="Btn2"
             onClick={() => {
-              setCount(1)
-              document.getElementsByClassName("lineSkillsColor")[0].style.display="none";
+              setCount(1);
+              document.getElementsByClassName(
+                "lineSkillsColor"
+              )[0].style.display = "none";
             }}
           >
             Experience
@@ -88,22 +49,77 @@ const ContentPage3 = () => {
           <button
             onClick={() => {
               setCount(2);
-              document.getElementsByClassName("lineSkillsColor")[0].style.display="none";
+              document.getElementsByClassName(
+                "lineSkillsColor"
+              )[0].style.display = "none";
             }}
           >
             Education
           </button>
           <div className="SkillsPage3">
-            {TopicArr[Count].map((Curr , id) => {
-              // console.log(Curr);
+            {TopicArr[Count].map((Curr, id) => {
               return (
                 <>
-                  <p id="topic">{Curr.Topic} </p>
-                  <p>{Curr.SubTopic}</p>
-                  {/* <p id="topic">{}</p>
-                <p></p>
-                <p id="topic">{}</p>
-                <p></p> */}
+                  <p id="topic" className="topic">
+                    {Curr.Topic}
+                  </p>
+                  <p className="subtopic">
+                    {Curr.SubTopic}
+
+                    <div className="SkillsOnHover">
+                      <div className="triangle"></div>
+                      <div className="SkillsDisplay">
+                        <div>
+                          <div className="SkillsDisplayTopic">
+                            <div>{Curr.HoverTopic1}</div>
+                            <div>{Curr.HoverTopic1Perc}</div>
+                          </div>
+                          <div id="SkillsLine">
+                            <div
+                              className="SkillsLineInner"
+                              style={{ width: Curr.HoverTopic1Perc}}
+                            ></div>
+                          </div>
+                        </div>
+                        <div>
+                          <div className="SkillsDisplayTopic">
+                            <div>{Curr.HoverTopic2}</div>
+                            <div>{Curr.HoverTopic2Perc}</div>
+                          </div>
+                          <div id="SkillsLine">
+                            <div
+                              className="SkillsLineInner"
+                              style={{ width: Curr.HoverTopic2Perc }}
+                            ></div>
+                          </div>
+                        </div>
+                        <div>
+                          <div className="SkillsDisplayTopic">
+                            <div>{Curr.HoverTopic3}</div>
+                            <div>{Curr.HoverTopic3Perc}</div>
+                          </div>
+                          <div id="SkillsLine">
+                            <div
+                              className="SkillsLineInner"
+                              style={{ width: Curr.HoverTopic3Perc}}
+                            ></div>
+                          </div>
+                        </div>
+                        <div>
+                          <div className="SkillsDisplayTopic">
+                            <div>{Curr.HoverTopic4}</div>
+                            <div>{Curr.HoverTopic4Perc}</div>
+                          </div>
+                          <div id="SkillsLine">
+                            <div
+                              className="SkillsLineInner"
+                              style={{ width: Curr.HoverTopic4Perc}}
+                            ></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </p>
                 </>
               );
             })}
@@ -117,7 +133,9 @@ const ContentPage3 = () => {
           {/* <p>Buliding Android Apps</p> */}
         </div>
       </div>
-      <a href="#home" id="ToTopBtn"><i class="fa-solid fa-arrow-up"></i></a>
+      <a href="#home" id="ToTopBtn">
+        <i class="fa-solid fa-arrow-up"></i>
+      </a>
     </div>
   );
 };
